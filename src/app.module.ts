7208efd13397dfab73436e130b69557e9af5cda7
erkinchik <erkin.berdibaev@gmail.com>
2023-11-25 21:59:57 +0200

    add module telegram and settings plus configs

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TelegramModule } from './telegram/telegram.module';
+import { getTelegramConfig } from './configs/telegram.config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    TelegramModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: getTelegramConfig,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
